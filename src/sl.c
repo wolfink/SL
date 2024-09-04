741ac09c36c014f2dd051946035e13f3518e2aa8
Max Sherr <maximiliansherr@gmail.com>
2024-09-04 14:12:36 -0400

    Allow for multiple macros (max 1024)

--- src/sl.c
+++ src/sl.c
@@ -12,66 +12,97 @@
 #define sal_error_msg(fmt,...) eprintf(__FILE__ ":%d: " fmt, __LINE__, ##__VA_ARGS__)
 #define assert(expr,...) if(!(expr)) { sal_error_msg("failed assertion: %s\n", #expr); FAIL }
 #define MAX_LINE 256
+#define MAX_MACRO_NAME 64
+#define MAX_MACROS 1024
 
 struct stat st={0};
+
+int macro_list_len=0;
+
+typedef struct string {
+        int len;
+        char data[MAX_LINE];
+} sl_string;
+
+typedef struct macro {
+        sl_string name;
+        void (*replace)(FILE*);
+} macro;
+macro macro_list[MAX_MACROS];
+
+#define str_to_cstr()
 
 int main(int argc, char** argv)
 #define FAIL return EXIT_FAILURE;
 {
         assert(argc > 1)
         FILE* in=fopen(argv[1], "r");
+        // Create /tmp/sl directory if none exists
         if (stat("/tmp/sl", &st) == -1) {
                 mkdir("/tmp/sl", 0700);
         }
-        FILE* temp=fopen("/tmp/sl/temp.c", "w");
         FILE* out=fopen("/tmp/sl/out.s", "w");
         assert(in != NULL)
-        assert(temp != NULL)
         assert(out != NULL)
         char line[MAX_LINE];
-        char macro_name[MAX_LINE];
-        int macro_name_length=-1;
         int cpy_into_temp=0;
-        // Find a macro, copy its code into temp.c
+
+        sl_string temp_name;
+        FILE* temp;
+        // Find a macro, copy its code into a c file
         while (fgets(line, MAX_LINE-1, in) != NULL) {
                 // Match lines starting with ## as macro definitions
                 int i=0; if (line[i] == '#' && line[++i] == '#') {
                         if (cpy_into_temp) {
+                                macro* mp=&macro_list[macro_list_len-1];
+                                sl_string tempso_name;
+                                tempso_name.len=sprintf(tempso_name.data, "/tmp/sl/%s.so", mp->name.data);
+
+                                assert(fclose(temp) == EXIT_SUCCESS)
+                                pid_t p=fork();
+                                if (p == 0) {
+                                        execvp("gcc", (char*[MAX_LINE]){"gcc", "-shared", temp_name.data, "-o", tempso_name.data});
+                                }
+                                wait(NULL);
+                                void* shared_object_handle=dlopen(tempso_name.data, RTLD_NOW);
+                                assert(shared_object_handle != NULL);
+                                mp->replace=dlsym(shared_object_handle, "replace");
+                                assert(mp->replace != NULL);
                                 cpy_into_temp=0;
-                                break;
+                                continue;
+                        } else {
+                                // Create macro
+                                macro* mp=&macro_list[macro_list_len++];
+                                // Copy macro name
+                                while (iswspace(line[++i]));
+                                int j=0;
+                                while (isalnum(line[i]) || line[i] == '_') {
+                                        mp->name.data[j++]=line[i++];
+                                }
+                                mp->name.len=j;
+                                // Open macro temp file
+                                temp_name.len=sprintf(temp_name.data, "/tmp/sl/%s.c", mp->name.data);
+                                temp=fopen(temp_name.data, "w");
+                                assert(temp != NULL)
+                                cpy_into_temp=1;
                         }
-                        macro_name_length=0;
-                        while (iswspace(line[++i]));
-                        while (isalnum(line[i]) || line[i] == '_') {
-                                macro_name[macro_name_length++]=line[i++];
-                        }
-                        cpy_into_temp=1;
-                } else if (cpy_into_temp) fputs(line, temp);
-        }
-        assert(macro_name_length > 0)
-        assert(fclose(temp) == EXIT_SUCCESS)
-        pid_t p=fork();
-        if (p == 0) {
-                execvp("gcc", (char*[8]){"gcc", "-shared", "/tmp/sl/temp.c", "-o", "/tmp/sl/temp.so"});
-        }
-        wait(NULL);
-        void* handle=dlopen("/tmp/sl/temp.so", RTLD_NOW);
-        assert(handle != NULL);
-        int(*replace)(FILE*)=dlsym(handle, "replace");
-        assert(replace != NULL);
-        while (fgets(line, MAX_LINE-1, in) != NULL) {
-                for (int i=0; i < MAX_LINE && line[i] != '\0'; i++) {
-                        if (macro_name_length > 0
-                                && i + macro_name_length < MAX_LINE
-                                && strncmp(macro_name, line + i, macro_name_length) == 0) {
-                                replace(out);
-                                i += macro_name_length;
+                } else if (cpy_into_temp) {
+                        fputs(line, temp);
+                } else for (int i=0; i < MAX_LINE && line[i] != '\0'; i++) {
+                        // If there is a macro, check if line matches macro
+                        if (macro_list_len > 0) for (int mli=0; mli < macro_list_len; mli++) {
+                                macro* mp=&macro_list[mli];
+                                if (i + mp->name.len < MAX_LINE && strncmp(mp->name.data, line+i, mp->name.len) == 0) {
+                                        mp->replace(out);
+                                        i += mp->name.len;
+                                }
                         }
                         putc(line[i], out);
                 }
+
         }
-        assert(fclose(out) == EXIT_SUCCESS);
-        p=fork();
+        assert(fclose(out) == EXIT_SUCCESS)
+        pid_t p=fork();
         if (p == 0) {
                 execvp("as", (char*[8]){"as", "/tmp/sl/out.s", "-o", "/tmp/sl/out.o"});
         }
